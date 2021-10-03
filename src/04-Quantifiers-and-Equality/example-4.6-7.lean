9810b8901c0405fe4a0f41fe5fd6b9e81e62454b
Arthur Ryman <arthur.ryman@gmail.com>
2021-10-02 22:01:23 -0400

    Proved 4.6 exercise 6 and 7

--- src/04-Quantifiers-and-Equality/example-4.6-7.lean
+++ src/04-Quantifiers-and-Equality/example-4.6-7.lean
@@ -3,6 +3,24 @@
 import data.int.basic
 
 #check sub_self
+#check mul_comm
+#check eq.symm
+#check eq.trans
 
 example (x : ℤ) : x * 0 = 0 :=
-sorry
+have h : x - x = 0 := sub_self x,
+-- have h1 : 0 = x - x := eq.symm h,
+have h2 : x * 0 = x * 0 := eq.refl (x * 0),
+have h3 : x * 0 = x * (x - x), by rw h,
+have h4 : x * (x - x) = x * x - x * x := mul_sub x x x,
+have h5 : x * x - x * x = 0 := sub_self (x * x),
+have h6 : x * 0 = x * x - x * x := eq.trans h3 h4,
+show x * 0 = 0,
+from eq.trans h6 h5
+
+-- write the above proof using calc
+example (x : ℤ) : x * 0 = 0 :=
+calc
+  x * 0 =  x * (x - x)  : by rw (sub_self x)
+  ...   = x * x - x * x : mul_sub x x x
+  ...   = 0             : sub_self (x * x)
